69f9827eb1180c1fbe55539311318ff0e7a8079e
Dmytro <88480486+vokir10@users.noreply.github.com>
2022-08-02 13:45:06 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        nav: true,
+        items: 1,
+        navText : ["",""],
+        dots: true,
+        dotsData: true
+    });
+})
